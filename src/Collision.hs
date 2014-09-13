4e7790bc6c52c8dba2252f6e9de5e0437d32b517
Crockeo <crockeo@gmail.com>
2014-09-13 16:49:47 -0400

    Added some wires that contain the bounds (in CollisionRectangles) and checking if something collides with them.

--- src/Collision.hs
+++ src/Collision.hs
@@ -2,7 +2,11 @@
   This module provides the ability for the collision of any two objects to be
   reasoned about easily.
 -}
-module Collision where
+module Collision ( CollisionRectangle (..)
+                 , Collidable (..)
+                 , collides
+                 , uncurriedCollides
+                 ) where
 
 --------------------
 -- Global Imports --
@@ -23,20 +27,20 @@ instance Collidable CollisionRectangle where
 {-|
   Getting the location of the edges of a CollisionRectangle.
 -}
-topSide, botSide, leftSide, rightSide :: CollisionRectangle -> Float
-topSide   (CollisionRectangle (V2 _ y) (V2 _ h)) = y + h
-botSide   (CollisionRectangle (V2 _ y) _       ) = y
-leftSide  (CollisionRectangle (V2 x _) _       ) = x
-rightSide (CollisionRectangle (V2 x _) (V2 w _)) = x + w
+topSide, bottomSide, leftSide, rightSide :: CollisionRectangle -> Float
+topSide    (CollisionRectangle (V2 _ y) (V2 _ h)) = y + h
+bottomSide (CollisionRectangle (V2 _ y) _       ) = y
+leftSide   (CollisionRectangle (V2 x _) _       ) = x
+rightSide  (CollisionRectangle (V2 x _) (V2 w _)) = x + w
 
 {-|
   Checking if two collision rectangles intersect.
 -}
 collides' :: CollisionRectangle -> CollisionRectangle -> Bool
 collides' c1 c2 =
-  collidesVertical || collidesHorizontal
-  where collidesVertical   = topSide   c1 > botSide  c2 && botSide  c1 < topSide c2
-        collidesHorizontal = rightSide c1 > leftSide c2 && leftSide c1 < rightSide c2
+  collidesVertical && collidesHorizontal
+  where collidesVertical   = topSide   c1 > bottomSide c2 && bottomSide c1 < topSide   c2
+        collidesHorizontal = rightSide c1 > leftSide   c2 && leftSide   c1 < rightSide c2
 
 {-|
   A class for turning a given object into a CollisionRectangle.
@@ -47,6 +51,13 @@ class Collidable a where
 {-|
   Checking if two collidables intersect.
 -}
-collides :: Collidable a => a -> a -> Bool
+collides :: (Collidable a, Collidable b) => a -> b -> Bool
 collides c1 c2 =
+  collisionRectangle c1 `collides'` collisionRectangle c2
+
+{-|
+  An uncurried version of collides.
+-}
+uncurriedCollides :: (Collidable a, Collidable b) => (a, b) -> Bool
+uncurriedCollides (c1, c2) =
   collisionRectangle c1 `collides'` collisionRectangle c2
